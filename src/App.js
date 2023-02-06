8328b752dd9b82ea4189eacca964a030a2983a1f
Shadowjian <karakalel.adorable@gmail.com>
2023-02-06 23:50:58 +0800

    updated router set up to 6.4+

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer"
-import Home from "./layout/pages/Home"
-import Careers from "./layout/pages/Careers"
-import Recruitment from "./layout/pages/Recruitment"
-import About from "./layout/pages/About"
-import Contact from "./layout/pages/Contact"
+import RootLayout from "./layouts/RootLayout"
+import Home from "./pages/home/Home"
+import Careers from "./pages/careers/Careers"
+import Recruitment from "./pages/recruitment/Recruitment"
+// import About from "./pages/about/About"
+import Contact from "./pages/contact/Contact"
 import { ThemeProvider } from "@emotion/react"
-import { createTheme } from "@mui/material"
+import { createTheme, dividerClasses } from "@mui/material"
 
+// Setting up new Router
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements
+} from "react-router-dom"
+import AboutLayout from "./layouts/AboutLayout"
+import CompanyProfile from "./components/Aboutpage/components/CompanyProfile"
+import Overview from "./components/Aboutpage/components/Overview"
+import Whatcanwedo from "./components/Aboutpage/components/Whatcanwedo"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import AdminLayout from "./layouts/AdminLayout"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="careers" element={<Careers />} />
+
+        <Route path="recruitment" element={<Recruitment />} />
+        <Route path="tab1" />
+
+        <Route path="about" element={<AboutLayout />}>
+          <Route path="profile" element={<CompanyProfile />} />
+          <Route path="overview" element={<Overview />} />
+          <Route path="whatwedo" element={<Whatcanwedo />} />
+        </Route>
+
+        <Route path="contact-us" element={<Contact />} />
+      </Route>
+      <Route path="admin" element={<AdminLayout />} />
+    </>
+  )
+)
 
 function App() {
-
   const theme = createTheme({
     typography: {
-            fontFamily: [
-               'Zen Old Mincho', 
-               'serif'
-            ].join(','),
-          }
-
+      fontFamily: ["Zen Old Mincho", "serif"].join(",")
+    }
   })
 
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="careers" element={<Careers />} />
-        <Route path="recruitment" element={<Recruitment />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact-us" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+      <RouterProvider router={router} />
     </ThemeProvider>
   )
 }
---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material"
+import React from "react"
+
+export default function AdminDashboard() {
+  return (
+    <Container>
+      <h1>Admin Dashboard</h1>
+    </Container>
+  )
+}
