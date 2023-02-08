f16b9c5ae8166d592d2eca55db5a4abab5c260b9
jeraxProg <113060248+jeraxProg@users.noreply.github.com>
2023-02-08 23:12:35 +0800

    new update Homepage and nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import { Menu as MenuIcon } from "@mui/icons-material"
-import { AppBar, Toolbar, Typography, styled, Menu } from "@mui/material"
+import { AppBar, Toolbar, Typography, styled, Menu, Avatar } from "@mui/material"
 import { Box } from "@mui/system"
 import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
+import LogoGif from "../../assets/images/hasolution.png";
 
 const Navbar = () => {
   const StyledToolbar = styled(Toolbar)({
@@ -18,10 +19,8 @@ const Navbar = () => {
 
   const MenuTypeItem = styled(Typography)({
     color: "white",
+
     "&:hover": {
-      color: "#D8AE5E"
-    },
-    "&:active": {
       color: "#D8AE5E"
     },
     fontWeight: "100"
@@ -38,41 +37,51 @@ const Navbar = () => {
       backgroundColor: "#0B3749",
       borderRadius: "5px"
     },
-    "&:active": {
-      color: "#D8AE5E"
-    },
     fontWeight: "bold",
     margin: "50px"
   })
 
   const [open, SetOpen] = useState(false)
 
+  const navlinkStyles = ({isActive}) =>{
+    return {
+      backgroundColor: isActive ? 'orange' : '',
+      borderRadius: isActive ? '10px' : '',
+      padding: isActive ? '5px 15px' : '',
+      
+    }
+  }
+
   return (
     <>
       <AppBar
         position="sticky"
-        sx={{ backgroundColor: "#0B3749", padding: "20px", opacity: "1" }}
+        sx={{ backgroundColor: "#0B3749", padding: "20px", opacity: "1"}}
       >
         <StyledToolbar>
           <Box>
-            <NavLink className="nav-link" to="/" c>
-              <MenuLogo>ＨＡソリューション株式会社</MenuLogo>
+            <NavLink to="/" >
+              <Box sx={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+               <Avatar alt="hasolution" src={LogoGif} sx={{ width: 60, height: 60 }}/>
+              <MenuLogo>ソリューション株式会社</MenuLogo>
+              </Box>
+             
             </NavLink>
           </Box>
           <MenuBox sx={{ display: { xs: "none", sm: "none", md: "flex" } }}>
-            <NavLink className="nav-link" to="/">
+            <NavLink style={navlinkStyles} to="/">
               <MenuTypeItem>Home</MenuTypeItem>
             </NavLink>
-            <NavLink className="nav-link" to="careers">
+            <NavLink style={navlinkStyles} to="careers">
               <MenuTypeItem>Career</MenuTypeItem>
             </NavLink>
-            <NavLink className="nav-link" to="recruitment">
+            <NavLink style={navlinkStyles} to="recruitment">
               <MenuTypeItem>Recruitment Information</MenuTypeItem>
             </NavLink>
-            <NavLink className="nav-link" to="about">
+            <NavLink style={navlinkStyles} to="about">
               <MenuTypeItem>About Company</MenuTypeItem>
             </NavLink>
-            <NavLink className="nav-link" to="contact-us">
+            <NavLink style={navlinkStyles} to="contact-us">
               <MenuTypeItem>Inquiry</MenuTypeItem>
             </NavLink>
           </MenuBox>
@@ -99,19 +108,19 @@ const Navbar = () => {
           }}
         >
           <Box sx={{ width: 350, height: "50vh", backgroundColor: "white" }}>
-            <NavLink className="nav-link" to="/">
+            <NavLink  to="/">
               <MenuTypeItem2>Home</MenuTypeItem2>
             </NavLink>
-            <NavLink className="nav-link" to="careers">
+            <NavLink  to="careers">
               <MenuTypeItem2>Career</MenuTypeItem2>
             </NavLink>
-            <NavLink className="nav-link" to="recruitment">
+            <NavLink  to="recruitment">
               <MenuTypeItem2>Recruitment Information</MenuTypeItem2>
             </NavLink>
-            <NavLink className="nav-link" to="about">
+            <NavLink  to="about">
               <MenuTypeItem2>About Company</MenuTypeItem2>
             </NavLink>
-            <NavLink className="nav-link" to="contact-us">
+            <NavLink  to="contact-us">
               <MenuTypeItem2>Inquiry</MenuTypeItem2>
             </NavLink>
           </Box>
