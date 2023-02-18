5b5a9a5e80a28e7716de178d3b32ee16b61df3d0
jeraxProg <113060248+jeraxProg@users.noreply.github.com>
2023-02-18 23:01:42 +0800

    update

--- src/pages/careers/CareerDetails.jsx
+++ src/pages/careers/CareerDetails.jsx
@@ -1,18 +1,17 @@
 import {
   AccessTime,
   ArrowBack,
-  BeachAccess,
+
   Business,
   CurrencyYen,
-  Image,
   Language,
   People,
-  PeopleAlt,
+
   Place,
   Wc,
   Work,
   WorkspacePremium,
-  Workspaces
+
 } from "@mui/icons-material"
 import {
   Avatar,
@@ -24,8 +23,8 @@ import {
   ListItemText,
   Typography
 } from "@mui/material"
-import { Box, Stack } from "@mui/system"
-import { Link, useLoaderData, useParams } from "react-router-dom"
+import { Box } from "@mui/system"
+import { Link, useLoaderData } from "react-router-dom"
 import HeroImage from "../../assets/images/jobcoverdetails.png"
 import AdsImage from "../../assets/images/img.png"
 
@@ -144,7 +143,7 @@ export default function CareerDetails() {
                   </Typography>
                 </Box>
                 <Typography variant="h5" fontSize="20px" paddingLeft="55px">
-                  {" "}
+               
                   {career.location}
                 </Typography>
               </Box>
@@ -267,7 +266,7 @@ export default function CareerDetails() {
           <Box sx={{marginTop:"30px"}}>
             <Typography variant="h4" sx={{fontSize:{xs:"30px"}}}>Job Description</Typography>
             <Box sx={{boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",}}>
-            <Typography component="p" sx={{fontSize:{xs:"17px"}, padding: "20px"}}>{career.job_desc}</Typography>
+           <Typography  sx={{fontSize:{xs:"17px"}, padding: "20px"}}>{career.job_desc}</Typography>
             <Button
             href="https://forms.gle/HdafsVVzWQ418cXv8"
             target="_blank"
