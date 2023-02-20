9ef581831afd301c4689e982a4428bef4ba78565
jeraxProg <113060248+jeraxProg@users.noreply.github.com>
2023-02-20 13:38:56 +0800

    none edits

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,6 +1,6 @@
 import Backdrop from "@mui/material/Backdrop"
 import CircularProgress from "@mui/material/CircularProgress"
-import { Box } from "@mui/system"
+
 import { Container, Typography } from "@mui/material"
 
 export default function Loader() {
